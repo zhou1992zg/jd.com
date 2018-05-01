6213a20f105ea32b41cc7dbedc51ae3001abdb76
xun <981923250@qq.com>
2018-05-02 01:17:03 +0800

    增加了js特效 1，header部分 最上面的紫色图片的点击删除特效 2，下拉时搜索框固定在浏览器窗口top位置特效  但有bug 浏览器窗口在1440px时显示居中  再大或再小分辨率时候就未能居中 3，鼠标经过"手机京东"时显示二级菜单

---
+++ js/header.js
@@ -0,0 +1,104 @@
+var Btn=document.getElementById("btn");
+/**
+ * 这是一个点击btn添加class="dp_n"(属性名，属性值)的函数
+ * 无参数
+ */
+var Header_img=document.getElementById("header_img");
+function closeImg(){
+	Header_img.setAttribute("class","dp_n");
+}
+
+
+
+var show=document.getElementById("show");
+function showImg(){
+	show.setAttribute("class","mobile_show_box");
+}
+var show=document.getElementById("show");
+function outImg(){
+	show.setAttribute("class","mobile_show_box1");
+}
+
+
+
+
+
+// /**
+//  * 这是个倒计时器
+//  * @param {string} hover 倒计时？小时
+//  * @param {string} minntes 倒计时多少分钟
+//  * @param {number=} seconds 倒计时多少秒
+//  * @return {Object} 返回值描述
+//  */
+
+// var Hover=document.getElementById('hover').innerHTML;
+// var Minntes=document.getElementById('minntes').innerHTML;
+// var Seconds=document.getElementById('seconds').innerHTML;
+// Time=setInterval(function countDown(h,m,s){
+// 	var Time_h=Number(h)*60*60*1000;
+// 	var Time_m=Number(m)*60*1000;
+// 	var Time_s=Number(s)*1000;
+// 	var Time_he=Time_h+Time_s+Time_m;
+// 	if (Time_he>0) {
+// 		Time_he-=1;
+// 		console.log(Time_he);
+// 	}else {
+// 		clearInterval(Time);
+// 		Hover='00';
+// 		Minntes='00';
+// 		Seconds='00';
+// 	}
+// },1000)
+
+
+/**
+* 这是top悬停搜索框效果
+*
+ */
+var dd=document.getElementById('dd');
+var aa=document.getElementById('aa');
+var bb=document.getElementById('bb');
+	//滚动事件
+	window.onscroll=function(){
+		//alert(11);
+		//scrollTop值 兼容处理
+		var y=document.documentElement.scrollTop||window.pageYOffset;
+		//判断y>=200
+		if(y>=700){
+			//dd 定位改变
+			dd.style.position='fixed';
+			dd.style.top=52+'px';
+			dd.style.left=125+'px';
+			dd.style.backgroundColor='#fff';
+			dd.style.zIndex='99999999999';
+			aa.style.top=60+'px';
+			aa.style.left=320+'px';
+			aa.style.width=1190+'px';
+			dd.parentNode.style.width=100+'%';
+			dd.parentNode.style.height=50+'px';
+			dd.parentNode.style.borderBottom='2px solid red';
+			dd.parentNode.style.position='fixed';
+			dd.parentNode.style.backgroundColor='#fff';
+			dd.parentNode.style.zIndex='99999999999';
+		}else if(y<700){
+			//回到原来的位置
+			dd.style.position='static';
+			dd.style.top=0+'px';
+			dd.style.left=0+'px';
+			dd.style.zIndex='0';
+			aa.style.top=0+'px';
+			aa.style.left=0+'px';
+			dd.parentNode.style.width=0+'%';
+			dd.parentNode.style.height=52+'px';
+			dd.parentNode.style.borderBottom='0px solid red';
+			dd.parentNode.style.position='static';
+			dd.parentNode.style.zIndex='0';
+		}
+	}
+
+
+
+
+
+
+
